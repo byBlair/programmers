fad2d59e3d91fdddaf4b75f0c127252d90b10a6c
byBlair <126355414+byBlair@users.noreply.github.com>
2024-08-09 14:56:02 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,7 @@
+def solution(s):
+    length = len(s)
+    middle_index = length // 2
+    if length % 2 == 0:
+        return s[middle_index -1:middle_index + 1]
+    else :
+        return s[middle_index]

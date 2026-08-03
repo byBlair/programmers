70f2d88543737f0ef9ca36542eb842ae3c5148ed
Jihyun Kim <126355414+byBlair@users.noreply.github.com>
2026-08-03 12:57:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,13 +1,11 @@
 def solution(num):
     answer = 0
-    while num != 1: #1이 될때까지 연산해야하므로 while 문 사용
-        if num % 2 == 0 :
-            num //= 2
+    while num != 1:
+        if num % 2 == 0:
+            num = num // 2
         else :
             num = num * 3 + 1
         answer += 1
-        
         if answer == 500:
             return -1
-        
-    return answer 
+    return answer

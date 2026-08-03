d8f9283ea5842b8899aca11c4ca86f10d7ae8f18
Jihyun Kim <126355414+byBlair@users.noreply.github.com>
2026-08-03 11:01:51 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.43 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,7 +1,8 @@
 def solution(arr):
-    answer = [arr[0]]
-    for i in range(1, len(arr)):  # 두 번째 원소부터 비교 시작
-        if arr[i] != arr[i - 1]:  # 이전 원소와 다르면 추가
-            answer.append(arr[i])
+    answer = []
+    for i in arr:
+        if not answer:
+            answer.append(i)
+        elif i != answer[-1]:
+            answer.append(i)
     return answer
-        

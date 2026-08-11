935947622c232f5ff81661c95ccfc1065301c67e
Jihyun Kim <126355414+byBlair@users.noreply.github.com>
2026-08-11 15:53:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.09 ms, Memory: 30.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,15 @@
 def solution(participant, completion):
     answer = {}
+    #먼저 참가자들 갯수를 세고
     for i in participant:
         if i in answer :
             answer[i] += 1
         else:
             answer[i] = 1
+    # 딕셔너리에 1 저장 되어 있는거라 0이 나오는애들이 있겠지?
     for i in completion:
         answer[i] -= 1
+    #그리고 또 포문을 돌면서 i보다 작은 애를 반환
     for i in answer :
         if answer[i] > 0:
             return i

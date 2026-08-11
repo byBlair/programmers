d333afff9c2be2e79ba4f63125cf73c885546dc8
Jihyun Kim <126355414+byBlair@users.noreply.github.com>
2026-08-11 15:49:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.44 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,12 @@
 def solution(participant, completion):
-	# 리스트를 이름 순으로 정렬
-    participant.sort()
-    completion.sort()
-    for i in range(len(completion)):
-         # 순서가 맞지않으면 완주하지 못한 선수
-        if participant[i] != completion[i]:
-            return participant[i]
-    # 위에서 순서가 다 맞았다면 마지막 선수가 완주하지 못한 선수        
-    return participant[-1]
+    answer = {}
+    for i in participant:
+        if i in answer :
+            answer[i] += 1
+        else:
+            answer[i] = 1
+    for i in completion:
+        answer[i] -= 1
+    for i in answer :
+        if answer[i] > 0:
+            return i

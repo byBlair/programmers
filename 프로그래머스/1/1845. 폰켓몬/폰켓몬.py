37f7c43513a57b1bb8d798aa4f980c38a9517cc6
Jihyun Kim <126355414+byBlair@users.noreply.github.com>
2026-08-11 16:24:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.67 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,4 +1,4 @@
 def solution(nums):
-    poketmon= len(set(nums))
-    max_select = len(nums) // 2
-    return min(poketmon, max_select)
+    answer = 0
+    answer = min(len(nums) // 2, len(set(nums)))
+    return answer

c445e9c36b2f2472170af65fd04218b0148c1486
Jihyun Kim <126355414+byBlair@users.noreply.github.com>
2026-09-08 15:30:12 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 0.73 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = []
+    last = {}
+    for i in range(len(s)):
+        if s[i] not in last:
+            answer.append(-1)
+        else:
+            answer.append(i - last[s[i]])
+        last[s[i]] = i
+    return answer
+        
